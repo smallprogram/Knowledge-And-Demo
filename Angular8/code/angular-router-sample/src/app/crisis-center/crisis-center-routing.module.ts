6d4b5a1d87b147ba9bd5cf1a4f9504bc1e35f552
zhusir <smallprogram@foxmail.com>
2020-05-25 01:04:14 +0800

    update

--- Angular8/code/angular-router-sample/src/app/crisis-center/crisis-center-routing.module.ts
+++ Angular8/code/angular-router-sample/src/app/crisis-center/crisis-center-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
+import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
 
 
 
 const routes: Routes = [
-
+  {
+    path: 'crisis-center',
+    component: CrisisCenterComponent,
+    children: [
+      {
+        path: '',
+        component: CrisisListComponent,
+        children: [
+          {
+            path: ':id',
+            component: CrisisDetailComponent
+          },
+          {
+            path: '',
+            component: CrisisCenterHomeComponent
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- Angular8/code/angular-router-sample/src/app/crisis-center/crisis-center.module.ts
+++ Angular8/code/angular-router-sample/src/app/crisis-center/crisis-center.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 
 
 
 
 @NgModule({
   declarations: [
-
-  CrisisCenterComponent],
+  CrisisCenterComponent,
+  CrisisCenterHomeComponent,
+  CrisisListComponent,
+  CrisisDetailComponent
+],
   imports: [
     CommonModule,
     FormsModule,
