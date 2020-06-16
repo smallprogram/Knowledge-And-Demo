1ee0104320fa5cbaa8c0b36d255a7faf3d02093b
zhusir <smallprogram@foxmail.com>
2020-06-16 15:39:47 +0800

    add ApiController and two GetAction

---
+++ RESTfulAPI/code/RESTfulApi/RESTfulApi.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTfulApi.Api.Services;
+
+namespace RESTfulApi.Api.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepositroy _companyRepositroy;
+
+        public CompaniesController(ICompanyRepositroy companyRepositroy)
+        {
+            _companyRepositroy = companyRepositroy ?? throw new ArgumentNullException(nameof(companyRepositroy));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()  //ActionResult<IEnumerable<DtoCompany>
+        {
+            var companies = await _companyRepositroy.GetCompaniesAsync();
+
+            return Ok(companies);
+        }
+
+        [HttpGet("{companyId}")]  // "api/Companies/{companyId}"
+        public async Task<IActionResult> GetCompany(Guid companyId)
+        {
+            var company = await _companyRepositroy.GetCompanyAsync(companyId);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            
+            return Ok(company);
+        }
+    }
+}
