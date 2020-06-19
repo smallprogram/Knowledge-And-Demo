9a2ee5d21ce17ce872c435209793701f3b9a27f7
zhusir <smallprogram@foxmail.com>
2020-06-19 22:57:41 +0800

    编写自定义的参数绑定方法，实现集合添加与集合获取

--- RESTfulAPI/code/RESTfulApi/RESTfulApi.Api/Controllers/CompanyCollectionsController.cs
+++ RESTfulAPI/code/RESTfulApi/RESTfulApi.Api/Controllers/CompanyCollectionsController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RESTfulApi.Api.Entities;
+using RESTfulApi.Api.Helpers;
 using RESTfulApi.Api.Models;
 using RESTfulApi.Api.Services;
 
@@ -24,6 +25,28 @@ namespace RESTfulApi.Api.Controllers
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        [HttpGet("({ids})",Name = nameof(GetCompanyCollection))]
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> GetCompanyCollection(
+            [FromRoute]
+            [ModelBinder(BinderType =typeof(ArrayModelBinder))]
+            IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+            var entities = await _companyRepositroy.GetCompaniesAsync(ids);
+            if (ids.Count() != entities.Count())
+            {
+                return NotFound();
+            }
+
+            var dtoToReturn = _mapper.Map<IEnumerable<CompanyDto>>(entities);
+
+            return Ok(dtoToReturn);
+        }
+
+
         [HttpPost]
         public async Task<ActionResult<IEnumerable<CompanyDto>>> CreateCompanyCollection([FromBody] IEnumerable<CompanyAddDto> companyCollection)
         {
@@ -38,11 +61,13 @@ namespace RESTfulApi.Api.Controllers
             await _companyRepositroy.SaveAsync();
 
 
-            return Ok();
+            //return Ok();
 
-            //var returnDto = _mapper.Map<CompanyDto>(entity);
+            var returnDto = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
 
-            //return CreatedAtRoute(nameof(GetCompany), new { companyId = returnDto.Id }, returnDto);
+            var idsString = string.Join(",", returnDto.Select(x => x.Id));
+
+            return CreatedAtRoute(nameof(GetCompanyCollection), new { ids = idsString }, returnDto);
         }
     }
 }
---
+++ RESTfulAPI/code/RESTfulApi/RESTfulApi.Api/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RESTfulApi.Api.Helpers
+{
+    /// <summary>
+    /// 自定义的绑定器
+    /// </summary>
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            // 如果需要绑定的数据类型不是IEnumerable，则绑定失败，并返回
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+            // 获取需要绑定的值
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+            // 如果绑定的值为null或者空格，则绑定成功返回
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            // 获取第一个绑定参数类型
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+            // 生成一个该类型的转换器
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            // 将需要绑定的值通过转换器转换为参数类型的值
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => converter.ConvertFromString(x.Trim())).ToArray();
+
+            // 生成一个空的绑定值类型的数组
+            var typeValues = Array.CreateInstance(elementType, values.Length);
+
+            // 将转换后的绑定类型的值复制进数组
+            values.CopyTo(typeValues, 0);
+
+            // 将该数组绑定
+            bindingContext.Model = typeValues;
+
+            // 设置绑定成功，并返回
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
