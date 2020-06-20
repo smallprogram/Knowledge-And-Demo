e53edd59f023f532ce033813019cc3630b9e87ee
zhusir <smallprogram@foxmail.com>
2020-06-20 14:40:30 +0800

    add Fluentivalidation package,not complate

---
+++ RESTfulAPI/code/RESTfulApi/RESTfulApi.Api/Fluentivalidations/EmployeeAddDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using RESTfulApi.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulApi.Api.Fluentivalidations
+{
+    public class EmployeeAddDtoValidator : AbstractValidator<EmployeeAddDto>
+    {
+        public EmployeeAddDtoValidator()
+        {
+            RuleFor(employee => employee.EmployeeNo).NotNull().NotEmpty();
+        }
+    }
+}
