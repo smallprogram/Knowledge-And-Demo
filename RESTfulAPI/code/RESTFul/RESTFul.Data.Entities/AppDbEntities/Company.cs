9f7187de25384e29499b183bda34515801639dde
zhusir <smallprogram@foxmail.com>
2020-07-06 14:39:43 +0800

    update, add some lib,add dbcontext and dbEntities

---
+++ RESTfulAPI/code/RESTFul/RESTFul.Data.Entities/AppDbEntities/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTFul.Data.Entities.AppDbEntities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Industry { get; set; }
+
+        public string Product { get; set; }
+        public string Introduction { get; set; }
+
+        public DateTime BankruptTime { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ RESTfulAPI/code/RESTFul/RESTFul.Data.Entities/AppDbEntities/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTFul.Data.Entities.AppDbEntities
+{
+    public enum Gender
+    {
+        男 = 1,
+        女 = 2
+    }
+}
