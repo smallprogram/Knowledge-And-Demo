13ef27cd89188d9a287ec15b20d5cdb8e03c9e76
zhusir <smallprogram@foxmail.com>
2020-07-21 15:09:13 +0800

    Add IdentityServer4 data in memory with ClientCredential

---
+++ IdentityServer/code/Demo_Idp4/Demo_Idp4/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Idp4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                        new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { 
+                new ApiScope("Api1","My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            { 
+                //OAth2.0的ClientCredentials认证方式
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "Api1" }
+                }
+            };
+    }
+}
---
+++ IdentityServer/code/Demo_Idp4/Demo_Idp4/Startup.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Demo_Idp4
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer(options =>
+            {
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddDeveloperSigningCredential(); //开发证书，生产环境更换为AddSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseIdentityServer();
+
+
+            
+
+        }
+    }
+}
