b310e4b96e82eeb3ecebb56da85e849dd8f8f421
zhusir <smallprogram@foxmail.com>
2020-08-24 11:17:28 +0800

    Update. Add TodoList

---
+++ Blazor/code/BlazorSample/BlazorServer_TodoList/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServer_TodoList.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
