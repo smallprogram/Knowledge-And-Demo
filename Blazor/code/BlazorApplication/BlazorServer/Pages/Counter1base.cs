de37be7b3c3dcedfdc38f0934aecb65766940f1a
zhusir <smallprogram@foxmail.com>
2020-09-04 23:07:01 +0800

    添加两种UI与代码分离的方式

---
+++ Blazor/code/BlazorApplication/BlazorServer/Pages/Counter1base.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class Counter1base : ComponentBase
+    {
+        protected int currentCount1 = 0;
+
+        protected void IncrementCount1()
+        {
+            currentCount1++;
+        }
+    }
+}
--- Blazor/code/BlazorApplication/BlazorServer/Startup.cs
+++ Blazor/code/BlazorApplication/BlazorServer/Startup.cs
@@ -22,8 +22,6 @@ namespace BlazorServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -31,7 +29,6 @@ namespace BlazorServer
             services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
