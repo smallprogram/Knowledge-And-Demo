0193a803b2a75b2e2b981c2a54e9e244c865cbfc
zhusir <smallprogram@foxmail.com>
2020-09-04 23:41:43 +0800

    添加RESTful的Blazor的架构

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Model/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmployeeManagement.Model
+{
+    public class Department
+    {
+        public Guid DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Model
+{
+    public class Employee
+    {
+        public Guid EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public Department Department { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
