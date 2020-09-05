cc97311d47a96b105a3221da9452b9ef094f00e2
zhusir <smallprogram@foxmail.com>
2020-09-05 11:05:16 +0800

    添加DBContext，Migrations,update database

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Api/Migrations/20200905030211_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<Guid>(nullable: false),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    DateOfBrith = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    DepartmentId = table.Column<Guid>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { new Guid("cbcebfdc-d176-4045-a680-75d5893fe185"), "IT" },
+                    { new Guid("fa87c5b3-0bbb-4ddb-a4ee-33a7a875cfe9"), "HR" },
+                    { new Guid("53049207-ed06-4411-b920-9ab7de6b5c0e"), "Payroll" },
+                    { new Guid("8347d0d5-5087-44a9-b3e6-1fcbb2d399dc"), "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBrith", "DepartmentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { new Guid("e5122517-92e7-42da-a445-915de6ee9717"), new DateTime(1980, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("cbcebfdc-d176-4045-a680-75d5893fe185"), "David@pragimtech.com", "John", 0, "Hastings", "images/john.jpg" },
+                    { new Guid("72b544d4-b703-4e43-baa9-25612d6dd7bf"), new DateTime(1981, 12, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("fa87c5b3-0bbb-4ddb-a4ee-33a7a875cfe9"), "Sam@pragimtech.com", "Sam", 0, "Galloway", "images/sam.jpg" },
+                    { new Guid("6a39fe40-8a48-492f-b5af-de38a2946b91"), new DateTime(1979, 11, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("cbcebfdc-d176-4045-a680-75d5893fe185"), "mary@pragimtech.com", "Mary", 1, "Smith", "images/mary.jpg" },
+                    { new Guid("b15ace62-d94d-40fa-8e22-f048998ded80"), new DateTime(1982, 9, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("53049207-ed06-4411-b920-9ab7de6b5c0e"), "sara@pragimtech.com", "Sara", 1, "Longway", "images/sara.jpg" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,83 @@
+﻿using EmployeeManagement.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Employee> Employees { set; get; }
+        public DbSet<Department> Departments { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = Guid.Parse("cbcebfdc-d176-4045-a680-75d5893fe185"), DepartmentName = "IT" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = Guid.Parse("fa87c5b3-0bbb-4ddb-a4ee-33a7a875cfe9"), DepartmentName = "HR" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = Guid.Parse("53049207-ed06-4411-b920-9ab7de6b5c0e"), DepartmentName = "Payroll" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = Guid.Parse("8347d0d5-5087-44a9-b3e6-1fcbb2d399dc"), DepartmentName = "Admin" });
+
+            // Seed Employee Table
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = Guid.Parse("e5122517-92e7-42da-a445-915de6ee9717"),
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBrith = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                DepartmentId = Guid.Parse("cbcebfdc-d176-4045-a680-75d5893fe185"),
+                PhotoPath = "images/john.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = Guid.Parse("72b544d4-b703-4e43-baa9-25612d6dd7bf"),
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "Sam@pragimtech.com",
+                DateOfBrith = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                DepartmentId = Guid.Parse("fa87c5b3-0bbb-4ddb-a4ee-33a7a875cfe9"),
+                PhotoPath = "images/sam.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = Guid.Parse("6a39fe40-8a48-492f-b5af-de38a2946b91"),
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBrith = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                DepartmentId = Guid.Parse("cbcebfdc-d176-4045-a680-75d5893fe185"),
+                PhotoPath = "images/mary.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = Guid.Parse("b15ace62-d94d-40fa-8e22-f048998ded80"),
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBrith = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                DepartmentId = Guid.Parse("53049207-ed06-4411-b920-9ab7de6b5c0e"),
+                PhotoPath = "images/sara.jpg"
+            });
+        }
+    }
+}
