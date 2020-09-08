9955810166603be830522bd00a92f4463c099513
zhusir <smallprogram@foxmail.com>
2020-09-08 16:05:38 +0800

    Update Blazor Data Binding

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Colour { get; set; } = "background-color:white";
+
+        protected string Gender { get; set; } = "Male";
+    }
+}
