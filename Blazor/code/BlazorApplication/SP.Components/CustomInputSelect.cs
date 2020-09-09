a5e550683048bf2911b68b59c775c7205b5124b7
zhusir <smallprogram@foxmail.com>
2020-09-09 22:29:51 +0800

    update, Create Blazor Custom Component Lib

--- Blazor/code/BlazorApplication/SP.Components/CustomInputSelect.cs
+++ Blazor/code/BlazorApplication/SP.Components/CustomInputSelect.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EmployeeManagement.Web.Pages
+namespace SP.Components
 {
     public class CustomInputSelect<TValue> : InputSelect<TValue>
     {
