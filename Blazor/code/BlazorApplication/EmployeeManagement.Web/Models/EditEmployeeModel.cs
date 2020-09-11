c17ecdb42bb0c54202ed2c22c9ec2488effb3573
zhusir <smallprogram@foxmail.com>
2020-09-11 10:59:48 +0800

    Add CompareProperty Validation

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagement.Model;
+using EmployeeManagement.Model.CustomValidators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class EditEmployeeModel
+    {
+        public Guid EmployeeId { get; set; }
+        [Required(ErrorMessage = "姓必须要填写")]
+        [MinLength(2, ErrorMessage = "姓的长度不能小于2")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "名必须要填写")]
+        public string LastName { get; set; }
+        [EmailAddress(ErrorMessage = "Email格式不对！")]
+        [EmailDomainValidator(AllowedDomain = "gmail.com")]
+        public string Email { get; set; }
+        [CompareProperty(nameof(Email),ErrorMessage ="邮件地址输入不一致")]
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBrith { get; set; }
+        public Gender Gender { get; set; }
+        public Guid? DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
--- Blazor/code/BlazorApplication/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ Blazor/code/BlazorApplication/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Model;
+using EmployeeManagement.Web.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -13,7 +14,8 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IEmployeeService EmployeeService { set; get; }
 
-        public Employee Employee { set; get; } = new Employee();
+        private Employee Employee { set; get; } = new Employee();
+        public EditEmployeeModel EditEmployeeModel { set; get; } = new EditEmployeeModel();
 
         [Parameter]
         public string Id { get; set; }
@@ -28,6 +30,21 @@ namespace EmployeeManagement.Web.Pages
             Employee = await EmployeeService.GetEmployee(Id);
             Departments = (await DepartmentService.GetDepartments()).ToList();
             //DepartmentId = Employee.DepartmentId.ToString();
+            EditEmployeeModel.ConfirmEmail = Employee.Email;
+            EditEmployeeModel.DateOfBrith = Employee.DateOfBrith;
+            EditEmployeeModel.Department = Employee.Department;
+            EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            EditEmployeeModel.Email = Employee.Email;
+            EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            EditEmployeeModel.Gender = Employee.Gender;
+            EditEmployeeModel.LastName = Employee.LastName;
+            EditEmployeeModel.FirstName = Employee.FirstName;
+            EditEmployeeModel.PhotoPath = Employee.PhotoPath;
+        }
+
+        protected void HandleValidSubmit()
+        {
+
         }
     }
 }
