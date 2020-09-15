0945534b57e777a6d6cad1727a8d86c7e340224c
zhusir <smallprogram@foxmail.com>
2020-09-15 22:43:16 +0800

    Add IdentitySupport

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EmployeeManagement.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmployeeManagement.Web.Areas.Identity.IdentityHostingStartup))]
+namespace EmployeeManagement.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EmployeeManagementWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EmployeeManagementWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EmployeeManagementWebContext>();
+            });
+        }
+    }
+}
