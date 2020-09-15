b296e204de21ecd2a3a1c1b62db925a538b116ba
zhusir <smallprogram@foxmail.com>
2020-09-15 15:21:15 +0800

    父子组件Attribute的传递与设置

---
+++ Blazor/code/BlazorApplication/EmployeeManagement.Web/Pages/ParentComponentBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class ParentComponentBase : ComponentBase
+    {
+    }
+}
